cabc4f3dbe0f3116eae550057eaf262e0366d968
fadahunsiseyi <oluwaseyisamuel018@gmail.com>
2022-04-20 14:09:15 +0100

    test deployment

--- src/components/data/project.jsx
+++ src/components/data/project.jsx
@@ -2,7 +2,7 @@ export const ProjectData = [
     {
         id: '1',
         title: 'Random Project 1',
-        about: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab ipsum aliquam voluptatem earum a vero blanditiis possimus delectus hic dolorem.',
+        about: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab ipsum aliquam voluptatem earum a vero blanditiis possimus delectus hic dolorem see the deployed version.',
         languages: ['ReactJs', 'NodeJs'],
         demo: 'www.example.com',
         github: 'www.github.com',
